4fd785d8c7638e2283ffc7ff51205e0db03a633c
salmanmohebi <s.mohebi22@gmail.com>
2018-09-17 13:41:46 +0430

    Exercise 1

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -17,7 +17,7 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
+    theta = theta - (alpha/m)*sum((X*theta-y).*X)';
 
 
 
@@ -27,6 +27,7 @@ for iter = 1:num_iters
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
+    
 
 end
 
